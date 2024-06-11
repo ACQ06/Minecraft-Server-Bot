2da98a14ad3894b88aaa08413a55594894da34d5
ACQ06 <questeriaalven@gmail.com>
2024-06-12 04:18:54 +0800

    Git ignored and updated IP

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import discord
 from discord.ext import tasks
 from mcstatus import JavaServer
 import os
+from dotenv import load_dotenv
 from keep_alive import keep_alive
 
 class Bot:
     def __init__(self):
         self.client = discord.Client(intents=discord.Intents.default())
         self.mc_server = MCServer()
-        self.channel_id = os.getenv("channel")
+        self.channel_id = os.getenv('channel')
         self.channel = self.client.get_channel(int(self.channel_id))
 
     def run_bot(self):
-        TOKEN = os.getenv("token")
+        TOKEN = os.getenv('token')
 
         @self.client.event
         async def on_ready():
@@ -36,20 +37,34 @@ class Bot:
                 await self.send_embed(self.channel)
             except Exception as e:
                 print(f"An error occurred in the update_status task: {e}")
-        
 
     async def send_embed(self, channel, mc_server, existing_embed=None):
-        address = mc_server.get_address()
+        embed = discord.Embed(
+            title="Minecraft Server [Online]",
+            description="ChennyCakes Server",
+            color=discord.Color.green())
+        
         version = mc_server.get_version()
+
+        if version == "Unknown":
+            print("Server Offline")
+            embed = discord.Embed(
+            title="Minecraf Server [Offline]",
+            description="KANGKONG CHIPS ORIGINAL BY JOSH MOJICA",
+            color=discord.Color.red())
+            if existing_embed:
+                await existing_embed.edit(embed=embed)
+                return
+            else:
+                print("Sending New Message")
+                await channel.send(embed=embed)
+                return 
+        print("Server Online")
+        address = mc_server.get_address()
         platform = mc_server.get_platform()
         player_count = mc_server.get_player_count()
         players = mc_server.get_players()
         ping = mc_server.get_ping()
-
-        embed = discord.Embed(
-            title="Minecraft Server",
-            description="KANGKONG CHIPS ORIGINAL BY JOSH MOJICA",
-            color=discord.Color.green())
 
         embed.add_field(name="Address", value=address, inline=False)
         embed.add_field(name="Online Players", value=players, inline=False)
@@ -60,13 +75,17 @@ class Bot:
         embed.set_footer(text=f"Server Ping: {ping} • ACQ")
 
         if existing_embed:
-            print(existing_embed.embeds[0].fields[1].value)
-            if existing_embed.embeds[0].fields[1].value != players:
+            existing_title = existing_embed.embeds[0].title
+            print(existing_title)
+            if existing_title == "Minecraft Server [Offline]":
                 print("Updating Message")
                 await existing_embed.edit(embed=embed)
-                return
             else:
-                print("No Update")
+                if existing_embed.embeds[0].fields[1].value != players:
+                    print("Updating Message")
+                    await existing_embed.edit(embed=embed)
+                else:
+                    print("No Update")
         else:
             print("Sending New Message")
             await channel.send(embed=embed)
@@ -74,9 +93,8 @@ class Bot:
 
 
 class MCServer:
-
     def __init__(self):
-        self.address = os.getenv("address")
+        self.address = os.getenv('address')
         self.SERVER = JavaServer.lookup(self.address)
 
     def get_address(self):
@@ -93,8 +111,13 @@ class MCServer:
         return ""
 
     def get_version(self):
-        status = self.SERVER.status()
-        return status.version.name
+        try:
+            status = self.SERVER.status()
+            self.is_online = True
+            return status.version.name
+        except Exception:
+            self.is_online = False
+            return "Unknown"
 
     def get_player_count(self):
         status = self.SERVER.status()
@@ -107,8 +130,12 @@ class MCServer:
     
     def get_ping(self):
         return f"{int(self.SERVER.ping())} ms"
+    
+    def get_server_status(self):
+        return self.SERVER.status()
 
 
+load_dotenv()
 keep_alive()
 bot = Bot()
 bot.run_bot()
